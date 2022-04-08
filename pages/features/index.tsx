46910ba2780298a9c85eb8ca419827e0e97301b8
ste163 <samtedwards@pm.me>
2022-04-08 15:39:30 -0400

    setup page routing, removed starter code

--- pages/features/index.tsx
+++ pages/features/index.tsx
@@ -1,3 +1,12 @@
-const Features = () => <h1>Features</h1>
+import Head from "next/head";
 
-export default Features
+const Features = () => (
+  <>
+    <Head>
+      <title>Features | Vislit </title>
+    </Head>
+    <h1>Features</h1>{" "}
+  </>
+);
+
+export default Features;
