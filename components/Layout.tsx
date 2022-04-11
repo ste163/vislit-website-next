fd527a3202a128addac1a7a23f97a13f6b0ee513
ste163 <samtedwards@pm.me>
2022-04-11 18:30:47 -0400

    started nav styling

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,12 +11,20 @@ const Layout = ({ children }) => {
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav className="bg-gray">
-        <Link href="/">Home</Link>
-        <Link href="/features">Features</Link>
-        <Link href="/news">News</Link>
-        <Link href="/about">About</Link>
-        <Link href="/download">Download</Link>
+      <nav className="flex justify-center items-center text-lg bg-gray font-black tracking-wide min-h-[40px]">
+        <div className="mr-12">
+          <Link href="/">Logo</Link>
+          <span className="mx-5">
+            <Link href="/features">FEATURES</Link>
+          </span>
+          <Link href="/news">NEWS</Link>
+        </div>
+        <div>
+          <Link href="/about">ABOUT</Link>
+          <span className="mx-5">
+            <Link href="/download">DOWNLOAD</Link>
+          </span>
+        </div>
       </nav>
 
       <main className="grow mx-10">{children}</main>
