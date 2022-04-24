f6c502bd8c5e6cd429beac84c35eeb5ad5fc91ed
Sam <samtedwards@pm.me>
2022-04-24 19:49:53 -0400

    setup structure for mobile nav

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,54 +1,112 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import Logo from "./Logo";
 
+function debounce(fn: Function, ms: number) {
+  let timer: any;
+  function setDebounce() {
+    timer = null;
+    fn.apply(this, arguments);
+  }
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(setDebounce, ms);
+  };
+}
+
 const NavigationBar = () => {
-  // will need to dynamically change between mobile and desktop navbar
-  // potentially get the initial size then dynamically change
-  // or show/hide based on media queries (so no js)
   //dev.to/brandymedia/how-to-build-a-mobile-friendly-fully-responsive-website-navigation-using-tailwind-css-3ef9
-  //www.section.io/engineering-education/creating-a-responsive-navigation-bar-using-tailwind-css-and-javascript/
-  const router = useRouter();
+  const { asPath } = useRouter();
+  const [windowWidth, setWindowWidth] = useState<number | null>(null);
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState<boolean>(false);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  // track window width in state
+  useEffect(() => {
+    const debounceHandleResize = debounce(() => {
+      setWindowWidth(window.innerWidth);
+    }, 20);
+    window.addEventListener("resize", debounceHandleResize);
+    return () => {
+      window.removeEventListener("resize", debounceHandleResize);
+    };
+  }, []);
+
+  // handle change from mobile to desktop + resetting state
+  useEffect(() => {
+    if (windowWidth > 768) {
+      setIsMobile(false);
+      if (isHamburgerOpen) setIsHamburgerOpen(false);
+    } else {
+      if (!isMobile) setIsMobile(true);
+    }
+  }, [windowWidth]);
 
   return (
-    <nav className="flex justify-center items-center text-lg py-2 bg-gray font-black tracking-wide min-h-[46px]">
-      <div className="flex mr-12">
-        <Link href="/">
-          <div className="cursor-pointer">
-            <Logo />
-          </div>
-        </Link>
+    <nav
+      className={`flex justify-center text-lg py-2 bg-gray font-black tracking-wide min-h-[46px] ${
+        isMobile && isHamburgerOpen && "absolute h-full w-full items-start"
+      }`}
+    >
+      <div
+        className={`flex mx-8 ${isMobile && "w-full"} ${
+          isMobile && isHamburgerOpen && "flex-col w-full"
+        }`}
+      >
+        <div className="flex justify-between w-full">
+          <Link href="/">
+            <div className="cursor-pointer self-start">
+              <Logo />
+            </div>
+          </Link>
 
-        <div
-          className={`mx-5 nav-link ${
-            router.asPath === "/features" && "active-nav-link"
-          }`}
-        >
-          <Link href="/features">FEATURES</Link>
+          <button
+            className={`${isMobile ? "" : "hidden"}`}
+            onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}
+          >
+            Burger
+          </button>
         </div>
-        <div
-          className={`nav-link ${
-            router.asPath === "/news" && "active-nav-link"
-          }`}
-        >
-          <Link href="/news">NEWS</Link>
-        </div>
-      </div>
-      <div className="flex">
-        <div
-          className={`nav-link ${
-            router.asPath === "/about" && "active-nav-link"
-          }`}
-        >
-          <Link href="/about">ABOUT</Link>
-        </div>
-        <div
-          className={`mx-5 nav-link ${
-            router.asPath === "/download" && "active-nav-link"
-          }`}
-        >
-          <Link href="/download">DOWNLOAD</Link>
+
+        <div>
+          {isMobile ? (
+            isHamburgerOpen && <div className="text-right">Hamburger menu</div>
+          ) : (
+            <ul className="flex">
+              <li
+                className={`nav-link ${
+                  asPath === "/features" && "active-nav-link"
+                }`}
+              >
+                <Link href="/features">FEATURES</Link>
+              </li>
+
+              <li
+                className={`nav-link ${
+                  asPath === "/news" && "active-nav-link"
+                }`}
+              >
+                <Link href="/news">NEWS</Link>
+              </li>
+
+              <li
+                className={`nav-link ${
+                  asPath === "/about" && "active-nav-link"
+                }`}
+              >
+                <Link href="/about">ABOUT</Link>
+              </li>
+
+              <li
+                className={`nav-link ${
+                  asPath === "/download" && "active-nav-link"
+                }`}
+              >
+                <Link href="/download">DOWNLOAD</Link>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </nav>
