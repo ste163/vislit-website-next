bdb6c2590e4f43c4d14115eae0aa5db61942b2f1
Sam <samtedwards@pm.me>
2022-04-24 20:07:59 -0400

    added mobile nav with styling; needs burger icon

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -16,7 +16,6 @@ function debounce(fn: Function, ms: number) {
 }
 
 const NavigationBar = () => {
-  //dev.to/brandymedia/how-to-build-a-mobile-friendly-fully-responsive-website-navigation-using-tailwind-css-3ef9
   const { asPath } = useRouter();
   const [windowWidth, setWindowWidth] = useState<number | null>(null);
   const [isHamburgerOpen, setIsHamburgerOpen] = useState<boolean>(false);
@@ -70,43 +69,59 @@ const NavigationBar = () => {
         </div>
 
         <div>
-          {isMobile ? (
-            isHamburgerOpen && <div className="text-right">Hamburger menu</div>
-          ) : (
-            <ul className="flex">
+          <ul
+            className={`${isMobile ? "text-right" : "flex"} ${
+              // if we're mobile and the hamburger is open, show list as a column
+              // but if we're not mobile, show as row. If we're mobile, hide row
+              isMobile && isHamburgerOpen
+                ? "flex flex-col"
+                : !isMobile
+                ? "flex"
+                : "hidden"
+            }`}
+          >
+            {isMobile && (
               <li
-                className={`nav-link ${
-                  asPath === "/features" && "active-nav-link"
+                className={`nav-link mt-6 mb-2 ${
+                  asPath === "/" && "active-nav-link"
                 }`}
               >
-                <Link href="/features">FEATURES</Link>
+                <Link href="/">HOME</Link>
               </li>
+            )}
 
-              <li
-                className={`nav-link ${
-                  asPath === "/news" && "active-nav-link"
-                }`}
-              >
-                <Link href="/news">NEWS</Link>
-              </li>
+            <li
+              className={`nav-link ${
+                asPath === "/features" && "active-nav-link"
+              } ${isMobile ? "my-2" : "mx-4"}`}
+            >
+              <Link href="/features">FEATURES</Link>
+            </li>
 
-              <li
-                className={`nav-link ${
-                  asPath === "/about" && "active-nav-link"
-                }`}
-              >
-                <Link href="/about">ABOUT</Link>
-              </li>
+            <li
+              className={`nav-link ${asPath === "/news" && "active-nav-link"} ${
+                isMobile ? "my-2" : "mx-4"
+              }`}
+            >
+              <Link href="/news">NEWS</Link>
+            </li>
 
-              <li
-                className={`nav-link ${
-                  asPath === "/download" && "active-nav-link"
-                }`}
-              >
-                <Link href="/download">DOWNLOAD</Link>
-              </li>
-            </ul>
-          )}
+            <li
+              className={`nav-link ${
+                asPath === "/about" && "active-nav-link"
+              } ${isMobile ? "my-2" : "mx-4"}`}
+            >
+              <Link href="/about">ABOUT</Link>
+            </li>
+
+            <li
+              className={`nav-link ${
+                asPath === "/download" && "active-nav-link"
+              } ${isMobile ? "my-2" : "mx-4"}`}
+            >
+              <Link href="/download">DOWNLOAD</Link>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
